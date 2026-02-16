85ddc8474085d767f661278271ba8f8c3ac18ece
benjamalegni <lukabenjaminmalegni@gmail.com>
2026-02-16 16:24:18 +0200

    added warning message for Spanish language only and pruned unused files/folders

--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -1,4 +1,5 @@
 import { navigate } from "astro:transitions/client";
+import { useState } from "react";
 
 function handleLanguage(language: string){
 	switch(language){
@@ -12,15 +13,33 @@ function handleLanguage(language: string){
 }
 
 export default function LanguageSelector() {
+
+	const [notification, setNotification] = useState(false);
+
 	return (
 		<>
-			<select>
+			<select
+				onMouseEnter={() => setNotification(true)}
+				onMouseLeave={() => setNotification(false)}
+				onFocus={() => setNotification(true)}
+				onBlur={() => setNotification(false)}
+			>
 				<option value="es">Spanish</option>
 			</select>
 
-			<button className="button" type="button" onClick={() => handleLanguage("es")}>
+			<button className="button" type="button" onClick={
+				() => handleLanguage("es")
+				}>
 				Begin the CCNA learning journey!
 			</button>
+
+			<div
+				className={`notification ${notification ? "notification--visible" : ""}`}
+				role="status"
+				aria-live="polite"
+			>
+				<p>ⓘ For now, notes are only available in Spanish.</p>
+			</div>
 		</>
 	);
 }
